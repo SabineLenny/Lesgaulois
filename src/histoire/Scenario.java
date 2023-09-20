5b44693802b47fd4f3c6c6fe6af5255796051f46
SabineLenny <sbl4837a@ens.fsi>
2023-09-20 15:12:17 +0200

    1ercommit

---
+++ src/histoire/Scenario.java
@@ -0,0 +1,12 @@
+package histoire;
+
+import personnages.Gaulois;
+
+public class Scenario {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
---
+++ src/personnages/Romain.java
@@ -0,0 +1,39 @@
+package personnages;
+
+public class Romain {
+	private String nom;
+	private int force;
+	
+	public Romain(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + " »");
+	}
+	private String prendreParole() {
+		return "Le romain " + nom + " : ";
+	}
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup;
+		if (force > 0) {
+			parler("Aïe");
+		} else {
+			parler("J'abandonne...");
+		}
+		
+	}
+	public static void main(String[] args) {
+		Romain julius= new Romain("Julius", 10);
+		System.out.println(julius.prendreParole());
+		julius.parler("Ave moi !");
+		System.out.println("Premier coup !");
+		julius.recevoirCoup(9);
+		System.out.println("Deuxième coup !");
+		julius.recevoirCoup(2);
+		}
+}
